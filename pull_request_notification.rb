4520318c0679da9818871df1faef27a6b50d6df2
jhasatyam166 <jhasatyam166@gmail.com>
2023-03-27 01:23:33 +0530

    test

--- pull_request_notification.rb
+++ pull_request_notification.rb
@@ -6,7 +6,7 @@ end
 
 client = Slack::Web::Client.new
 
-message = "A review has been requested for the pull request: <#{ENV['PULL_REQUEST_URL']}|#{ENV['PULL_REQUEST_TITLE']}> by #{ENV['REQUESTED_REVIEWER']}"
+message = "A review has been requested for the pull request: <#{ENV['PULL_REQUEST_URL']}|#{ENV['PULL_REQUEST_TITLE']}> by <@#{ENV['REQUESTED_REVIEWER']}>"
 channel = ENV['SLACK_CHANNEL_ID']
 
 client.chat_postMessage(channel: channel, text: message)
