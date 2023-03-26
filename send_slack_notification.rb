7a3d8b1bb3c807bbbbd982b14359cb18b0c1325f
jhasatyam166 <jhasatyam166@gmail.com>
2023-03-26 23:59:13 +0530

    test

--- send_slack_notification.rb
+++ send_slack_notification.rb
@@ -8,6 +8,8 @@ client = Slack::Web::Client.new
 
 pull_request_url = ENV['GITHUB_SERVER_URL'] + '/' + ENV['GITHUB_REPOSITORY'] + '/pull/' + ENV['GITHUB_REF'].split('/').last
 message = "New comment on your pull request <#{pull_request_url}|#{ENV['GITHUB_HEAD_REF']}> by #{ENV['GITHUB_ACTOR']}: \"#{ENV['COMMENT_BODY']}\""
+
 channel = ENV['SLACK_CHANNEL_ID']
 
+puts "channel id #{channel}"
 client.chat_postMessage(channel: channel, text: message)
