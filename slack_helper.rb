ebe61ebc82d71ff513b543bd9dcac454051b435b
jhasatyam166 <jhasatyam166@gmail.com>
2023-03-28 04:39:02 +0530

    test

--- slack_helper.rb
+++ slack_helper.rb
@@ -4,6 +4,7 @@ def get_slack_user(github_username, client)
   response = RestClient.get("https://api.github.com/users/#{github_username}", { 'Accept': 'application/vnd.github.v3+json' })
   user_data = JSON.parse(response.body)
   email_address = user_data['email']
+  puts "user_data #{user_data}"
 
   result = client.users_lookupByEmail(email: email_address)
   puts "result #{result}"
