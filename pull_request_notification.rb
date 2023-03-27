c32aa1689f084358e2c8b4789fe88da8e71a9692
jhasatyam166 <jhasatyam166@gmail.com>
2023-03-28 03:31:18 +0530

    test

--- pull_request_notification.rb
+++ pull_request_notification.rb
@@ -1,4 +1,6 @@
 require 'slack'
+require_relative 'slack_helper'
+
 
 Slack.configure do |config|
   config.token = ENV['SLACK_BOT_TOKEN']
@@ -7,20 +9,10 @@ end
 client = Slack::Web::Client.new
 message = "A review has been requested for the pull request: <#{ENV['PULL_REQUEST_URL']}|#{ENV['PULL_REQUEST_TITLE']}> by <@#{ENV['REQUESTED_REVIEWER']}>"
 puts "message #{message}"
-slack_user = get_slack_user
+slack_user = get_slack_user(ENV['GITHUB_ACTOR'], client)
 puts "slack_user #{slack_user}"
 
 channel = ENV['SLACK_CHANNEL_ID']
 
 client.chat_postMessage(channel: channel, text: message)
 
-def get_slack_user
-  github_username = ENV['GITHUB_ACTOR']
-  response = RestClient.get("https://api.github.com/users/#{github_username}", { 'Accept': 'application/vnd.github.v3+json' })
-  user_data = JSON.parse(response.body)
-  email_address = user_data['email']
-
-  result = client.users_lookupByEmail(email: email_address)
-  puts "result #{result}"
-  slack_username = result.user.name
-end
---
+++ slack_helper.rb
@@ -0,0 +1,9 @@
+def get_slack_user(github_username, client)
+  response = RestClient.get("https://api.github.com/users/#{github_username}", { 'Accept': 'application/vnd.github.v3+json' })
+  user_data = JSON.parse(response.body)
+  email_address = user_data['email']
+
+  result = client.users_lookupByEmail(email: email_address)
+  puts "result #{result}"
+  slack_username = result.user.name
+end
