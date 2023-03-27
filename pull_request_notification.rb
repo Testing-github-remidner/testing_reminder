4e21ee412d56f4bd0e4cc56f2b105eade26123ed
jhasatyam166 <jhasatyam166@gmail.com>
2023-03-28 04:36:37 +0530

    test

--- pull_request_notification.rb
+++ pull_request_notification.rb
@@ -11,6 +11,8 @@ end
 client = Slack::Web::Client.new
 message = "A review has been requested for the pull request: <#{ENV['PULL_REQUEST_URL']}|#{ENV['PULL_REQUEST_TITLE']}> by <@#{ENV['REQUESTED_REVIEWER']}>"
 puts "message #{message}"
+
+puts "Gitjub actor #{ENV['GITHUB_ACTOR']}"
 slack_user = get_slack_user(ENV['GITHUB_ACTOR'], client)
 puts "slack_user #{slack_user}"
 
