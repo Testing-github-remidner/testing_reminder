22f922697d2db69ce97c9e13bdd4b854ceb8a219
jhasatyam166 <jhasatyam166@gmail.com>
2023-03-29 03:57:25 +0530

    test;

--- send_slack_notification.rb
+++ send_slack_notification.rb
@@ -13,7 +13,4 @@ puts "pull_request_url #{pull_request_url}"
 puts "message #{message}"
 puts "slack token #{ENV['SLACK_BOT_TOKEN']}"
 
-channel = ENV['SLACK_CHANNEL_ID']
 
-puts "channel id #{channel}"
-client.chat_postMessage(channel: channel, text: message)
